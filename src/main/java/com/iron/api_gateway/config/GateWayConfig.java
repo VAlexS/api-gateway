b961fa3e22be1d5160d5a89a239225d750a913fa
Víctor Sanz <vsanzc@alumnos.nebrija.es>
2025-05-22 13:27:21 +0200

    Configuracion agregada

--- src/main/java/com/iron/api_gateway/config/GateWayConfig.java
+++ src/main/java/com/iron/api_gateway/config/GateWayConfig.java
@@ -1,4 +1,20 @@
 package com.iron.api_gateway.config;
 
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class GateWayConfig {
+
+    @Bean
+    public RouteLocator customeRouteLocator(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route("grades", r -> r.path("/api/grades")
+                        .uri("lb://grades-data-service"))
+                .route("student", r -> r.path("/api/student/**")
+                        .uri("lb://student-info-service")
+                ).build();
+    }
 }
